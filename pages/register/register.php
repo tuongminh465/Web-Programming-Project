012f887ef40ab5da8ad6db636a7be7fd4136dd0c
Hien Vo <vpmhien2001@gmail.com>
2022-04-15 10:41:57 +0700

    modified .css and .php file

--- pages/register/register.php
+++ pages/register/register.php
@@ -11,7 +11,12 @@
 <body>
     <div class="form-container">
         <form action="" method="post" enctype="multipart/form-data"></form>  <!-- enctype: encoded before submit to server -->
-        <input type="text" placeholder="enter username">
+        <input type="text" name="name" placeholder="enter username" class="box" required>
+        <input type="password" name="password" placeholder="enter password" class="box" required>
+        <input type="password" name="cpassword" placeholder="confirm password" class="box" required>
+        <input type="file" class="box" accept="image=/jpg, image/jpeg, image/png">
+        <input type="submit" value="register now" class="btn">
+        <p>Already have an account? <a href="login.php">login now</a></p>
     </div>
 </body>
 </html>
