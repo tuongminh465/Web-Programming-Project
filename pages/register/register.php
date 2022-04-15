db6486b91816a9599e03fa5d0fa7be67f2f519ec
Hien Vo <vpmhien2001@gmail.com>
2022-04-15 09:48:22 +0700

    modified register.php file

--- pages/register/register.php
+++ pages/register/register.php
@@ -5,8 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>register</title>
+    <!-- css file link -->
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div class="form-container">
+        <form action="" method="post" enctype="multipart/form-data"></form>  <!-- enctype: encoded before submit to server -->
+        <input type="text" placeholder="enter username">
+    </div>
 </body>
 </html>
