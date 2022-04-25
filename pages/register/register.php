3361359bf478589c9421c1eb4f1175aec6ad4dfd
Hien Vo <vpmhien2001@gmail.com>
2022-04-25 08:38:46 +0700

    modified register.php file

--- pages/register/register.php
+++ pages/register/register.php
@@ -11,14 +11,15 @@
 <body>
     <div class="form-container">
         <h3>register now</h3>
-        <form action="" method="post" enctype="multipart/form-data"></form>  <!-- enctype: encoded before submit to server -->
-        <input type="text" name="name" placeholder="enter username" class="box" required>
-        <input type="email" name="email" placeholder="enter email" class="box" required>
-        <input type="password" name="password" placeholder="enter password" class="box" required>
-        <input type="password" name="cpassword" placeholder="confirm password" class="box" required>
-        <input type="file" class="box" accept="image=/jpg, image/jpeg, image/png">
-        <input type="submit" value="register now" class="btn">
-        <p>Already have an account? <a href="login.php">login now</a></p>
+        <form action="" method="post" enctype="multipart/form-data"> <!-- enctype: encoded before submit to server -->
+            <input type="text" name="name" placeholder="enter username" class="box" required>
+            <input type="email" name="email" placeholder="enter email" class="box" required>
+            <input type="password" name="password" placeholder="enter password" class="box" required>
+            <input type="password" name="cpassword" placeholder="confirm password" class="box" required>
+            <input type="file" class="box" accept="image=/jpg, image/jpeg, image/png">
+            <input type="submit" value="register now" class="btn">
+            <p>Already have an account? <a href="login.php">login now</a></p>
+        </form>  
     </div>
 </body>
 </html>
