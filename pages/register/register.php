97756368c067f9f5b2b53783450dc03e6c0b5ee1
Hien Vo <vpmhien2001@gmail.com>
2022-05-12 17:00:51 +0700

    add

--- pages/register/register.php
+++ pages/register/register.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'user_db') or die('connection failed');
+    $conn = mysqli_connect('localhost', 'root', 'root', 'user_db') or die('connection failed');
 
     if(isset($_POST['submit'])){    
 
